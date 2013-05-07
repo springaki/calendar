9401783ba3e79fe40896f7900f164b3ed48217b4
Aki <aki.onetime@gmail.com>
2013-05-08 08:27:27 +0900

    fix it comment.

--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -1,37 +1,41 @@
 require './calendar.rb'
 
 describe 'Calendar' do
-  context 'new parameter is 2013, 4' do
+  context 'new instance is 2013, 4' do
+    before do
+      @calendar = Calendar.new(2013, 4)
+    end
     describe '#week' do
-      context "1th weeb" do
-        it 'string is 2013/04 calendar' do
-          Calendar.new(2013, 4).week(1).should eq [nil, 1, 2, 3, 4, 5, 6]
+      context "with 1" do
+        it '1st week. nil and 1st to 6th days Array' do
+          @calendar.week(1).should eq [nil, 1, 2, 3, 4, 5, 6]
         end
       end
 
-      context "2nd week" do
-        it '7th to 13th Array' do
-          Calendar.new(2013, 4).week(2).should eq [7,8,9,10,11,12,13]
+      context "with 2" do
+        it '2nd week. 7th to 13th days Array' do
+          @calendar.week(2).should eq [7,8,9,10,11,12,13]
         end
       end
 
-      context "3rd week" do
-        it '14th to 20th Array' do
-          Calendar.new(2013, 4).week(3).should eq [14,15,16,17,18,19,20]
+      context "with 3" do
+        it '3rd week. 14th to 20th days Array' do
+          @calendar.week(3).should eq [14,15,16,17,18,19,20]
         end
       end
 
-      context "4th week" do
-        it '21th to 27th Array' do
-          Calendar.new(2013, 4).week(4).should eq [21,22,23,24,25,26,27]
+      context "with 4" do
+        it '4th week. 21th to 27th days Array' do
+          @calendar.week(4).should eq [21,22,23,24,25,26,27]
         end
       end
 
-      context "5th week" do
-        it '28th to 30th and nil Array' do
-          Calendar.new(2013, 4).week(5).should eq [28,29,30,nil,nil,nil,nil]
+      context "with 5" do
+        it '5th week. 28th to 30th days and nil Array' do
+          @calendar.week(5).should eq [28,29,30,nil,nil,nil,nil]
         end
       end
+
     end
   end
 end
